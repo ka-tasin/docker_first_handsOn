f3033db864818bb51f38edb5749b1d4adae10579
ka-tasin <kausarahmadtasinkat@gmail.com>
2026-02-21 14:07:15 +0600

    manager middleware added

---
+++ middleware/manager.go
@@ -0,0 +1,29 @@
+package middleware
+
+import "net/http"
+
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+type Manager struct {
+	globalMiddlwares []Middleware
+}
+
+func NewMiddleware() *Manager {
+	return &Manager{
+		globalMiddlwares: make([]Middleware, 0),
+	}
+}
+
+func (mngr *Manager) With(middlewares ...Middleware) Middleware {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		n := next
+
+		for i := len(middlewares) - 1; i >= 0; i-- {
+			middleware := middlewares[i]
+			n = middleware(n)
+		}
+
+		return n;
+ 	}
+	
+}
